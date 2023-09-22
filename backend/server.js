28de905e87b9d00878d9c4089b6063204dfae6a2
Kyran L <kyran.lewis14@gmail.com>
2023-09-22 13:17:41 +0100

    added new folder front-end-new that contains code for a standard js front end rather than react, added additional buttons to select a joke based on the type of joke

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,12 @@ const app = express()
 const port = 3000
 const mongoose = require('mongoose');
 const Joke = require('./models/jokesModel')
+const path = require('path')
 
 app.use(express.json())
+
+app.use(express.static(path.join(__dirname, '../front-end-new')));
+
 
 // allow access (cors)
 app.use(function(req, res, next) {
@@ -16,11 +20,19 @@ app.use(function(req, res, next) {
     next();
 });
 
+app.get('/',  (req, res) =>{
+    res.sendFile(path.join(__dirname,'../front-end-new/index.html'))
+})
+
 // get a random joke from database 
 app.get('/jokes', async(req, res) =>{
     try{
+        const generalJokes = await Joke.find({ "type": "general"})
+        const programmingJokes = await Joke.find({ "type": "programming"})
+        const knockKnockJokes = await Joke.find({ "type": "knock-knock"})
+
         const randomJoke = await Joke.aggregate([{ $sample: { size: 1 } }]) // Adapted from https://stackoverflow.com/questions/2824157/how-can-i-get-a-random-record-from-mongodb
-        res.status(200).json(randomJoke)
+        res.status(200).json([generalJokes,programmingJokes,knockKnockJokes])
     } catch(error){
         res.status(500).json({message: error.message})
     }
