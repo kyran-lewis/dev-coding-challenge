8cc8183f6b6678467f3fd7347c8c38a0d09a613a
Kyran L <kyran.lewis14@gmail.com>
2023-10-01 14:13:46 +0100

    refactored code to use ejs templating, changed styles to add more aesthetic look to the webpage, fade in animation for joke cards

--- backend/server.js
+++ backend/server.js
@@ -1,42 +1,54 @@
 // import necessary modules
 const express = require('express')
+const cors = require('cors')
+//const expressLayouts = require('express-ejs-layouts')
 const app = express()
 const port = 3000
 const mongoose = require('mongoose');
 const Joke = require('./models/jokesModel')
 const path = require('path')
+let jokeType = 0
 
 app.use(express.json())
 
 app.use(express.static(path.join(__dirname, '../front-end-new')));
+app.set('views',path.join(__dirname,'views'))
+app.set('view engine','ejs');
+
+app.use(cors());
 
 
-// allow access (cors)
-app.use(function(req, res, next) {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
-
-app.get('/',  (req, res) =>{
-    res.sendFile(path.join(__dirname,'../front-end-new/index.html'))
+app.get('/',  async(req, res) =>{
+    const generalJokes = await Joke.find({ "type": "general"})
+    let randomNum = Math.floor((Math.random() * generalJokes.length));
+    res.render('index', {
+        joke : generalJokes[randomNum]
+    })
 })
 
-// get a random joke from database 
-app.get('/jokes', async(req, res) =>{
-    try{
-        // find jokes by type
-        const generalJokes = await Joke.find({ "type": "general"})
-        const programmingJokes = await Joke.find({ "type": "programming"})
-        const knockKnockJokes = await Joke.find({ "type": "knock-knock"})
-        // send jokes as an array where general are at position 0, programming jokes at position 1, knock knock jokes are at position 2
-        res.status(200).json([generalJokes,programmingJokes,knockKnockJokes])
-    } catch(error){
-        res.status(500).json({message: error.message})
-    }
-})
+app.get("/general", async(req, res) => {
+    generalJokes = await Joke.find({ "type": "general"})
+    let randomNum = Math.floor((Math.random() * generalJokes.length));
+    res.render('partials/joke-card', {
+      joke: generalJokes[randomNum]
+    });
+  });
+  
+  app.get("/programming", async(req, res) => {
+    const programmingJokes = await Joke.find({ "type": "programming"}),
+    randomNum = Math.floor((Math.random() * programmingJokes.length));
+    res.render('partials/joke-card', {
+      joke: programmingJokes[randomNum]
+    });
+  });
+
+  app.get("/knock-knock", async(req, res) => {
+    const knockKnockJokes = await Joke.find({ "type": "knock-knock"}),
+    randomNum = Math.floor((Math.random() * knockKnockJokes.length));
+    res.render('partials/joke-card', {
+      joke: knockKnockJokes[randomNum]
+    });
+  });
 
 // connect to db
 mongoose.set('strictQuery',false)
